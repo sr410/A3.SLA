3342057bf469b6ff284285516e092c857d38b06f
sr410 <sukhmanjeet.rekhi@ontariotechu.net>
2025-11-23 22:29:04 -0500

    Made changes to index.js

--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,7 @@ router.get('/', (req, res) => {
   res.render('index', { title: 'Home' });
 });
 
-/* Home page (same as /) */
+/* Homepage (same as /) */
 router.get('/home', (req, res) => {
   res.render('index', { title: 'Home' });
 });
